e2cac84167ee53e953237d1beba5499a99811ef1
Yuriy Homyakov <yuriy.homyakov@jibrel.network>
2017-11-20 17:11:28 +0000

    custom account adapter

---
+++ jco/api/utils.py
@@ -0,0 +1,12 @@
+from allauth.account.adapter import DefaultAccountAdapter
+
+
+class AccountAdapter(DefaultAccountAdapter):
+
+    def get_email_confirmation_url(self, request, emailconfirmation):
+        """Constructs the email confirmation (activation) url.
+        Note that if you have architected your system such that email
+        confirmations are sent outside of the request context `request`
+        can be `None` here.
+        """
+        return super().get_email_confirmation_url(None, emailconfirmation)
