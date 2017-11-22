98820b531f578fc705d6d8f4d8252174c380ea6f
Victor Mezrin <mezrinv@gmail.com>
2017-11-22 17:25:54 +0000

    add file with all email subjects

---
+++ jco/appprocessor/templates/subjects.py
@@ -0,0 +1,13 @@
+account_01 = "email subject"
+account_02 = "email subject"
+
+kyc_01 = "email subject"
+kyc_02 = "email subject"
+
+registration_01 = "email subject"
+registration_02 = "email subject"
+
+transactions_01 = "email subject"
+transactions_02 = "email subject"
+transactions_03 = "email subject"
+transactions_04 = "email subject"
