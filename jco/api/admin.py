6e270885de549268c2b8cb1e08a0562edfce3a48
Yuriy Homyakov <yuriy.homyakov@jibrel.network>
2017-11-27 16:39:52 +0000

    better account admin

--- jco/api/admin.py
+++ jco/api/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from django.utils.html import format_html
 from django.core.urlresolvers import reverse
-from django.shortcuts import redirect, get_object_or_404
+from django.shortcuts import redirect, get_object_or_404, render
 from django.conf.urls import url
 from django.contrib import messages
 from django.views.decorators.http import require_http_methods
@@ -18,12 +18,11 @@ from jco.api.models import Address, Account, Transaction, Jnt, Withdraw
 @admin.register(Account)
 class AccountAdmin(admin.ModelAdmin):
     list_display = ['id', 'username', 'first_name', 'last_name', 'document_thumb',
-                    'onfido_applicant_id', 'onfido_check_id', 'onfido_check_created',
-                    'onfido_check_status', 'onfido_check_result',
+                    'onfido_check_status',
                     'is_identity_verified', 'is_identity_verification_declined', 'account_actions']
 
     list_filter = ['is_identity_verified', 'is_identity_verification_declined',
-                   'onfido_check_status', 'onfido_check_result']
+                   'onfido_check_status']
 
     search_fields = ['user__username', 'first_name', 'last_name']
 
@@ -37,20 +36,25 @@ class AccountAdmin(admin.ModelAdmin):
     def get_urls(self):
         urls = super().get_urls()
         custom_urls = [
+            # url(
+            #     r'^(?P<account_id>.+)/reset/$',
+            #     self.admin_site.admin_view(self.reset_identity_verification),
+            #     name='account-reset',
+            # ),
+            # url(
+            #     r'^(?P<account_id>.+)/approve/$',
+            #     self.admin_site.admin_view(self.approve_identity_verification),
+            #     name='account-approve',
+            # ),
+            # url(
+            #     r'^(?P<account_id>.+)/decline/$',
+            #     self.admin_site.admin_view(self.decline_identity_verification),
+            #     name='account-decline',
+            # ),
             url(
-                r'^(?P<account_id>.+)/reset/$',
-                self.admin_site.admin_view(self.reset_identity_verification),
-                name='account-reset',
-            ),
-            url(
-                r'^(?P<account_id>.+)/approve/$',
-                self.admin_site.admin_view(self.approve_identity_verification),
-                name='account-approve',
-            ),
-            url(
-                r'^(?P<account_id>.+)/decline/$',
-                self.admin_site.admin_view(self.decline_identity_verification),
-                name='account-decline',
+                r'^(?P<account_id>.+)/action/$',
+                self.admin_site.admin_view(self.account_action),
+                name='account-action',
             ),
         ]
         return custom_urls + urls
@@ -67,14 +71,11 @@ class AccountAdmin(admin.ModelAdmin):
 
     def account_actions(self, obj):
         return format_html(
-            '<form></form><form style="display:inline" method="post" action="{}"><input type="hidden" name="csrfmiddlewaretoken" value="{token}"><button class="button">Reset</button></form>&nbsp;'
-            '<form method="post" style="display:inline" action="{}"><input type="hidden" name="csrfmiddlewaretoken" value="{token}"><button class="button">Approve</button></form>&nbsp;'
-            '<form method="post" style="display:inline" action="{}"><input type="hidden" name="csrfmiddlewaretoken" value="{token}"><button class="button">Decline</button></form>&nbsp;',
-            reverse('admin:account-reset', args=[obj.pk]),
-            reverse('admin:account-approve', args=[obj.pk]),
-            reverse('admin:account-decline', args=[obj.pk]),
-            token=get_token(self.request)
-        )
+            '<a class="button" href="{url}?action=reset">Reset</a>&nbsp;'
+            '<a class="button" href="{url}?action=approve">Approve</a>&nbsp;'
+            '<a class="button" href="{url}?action=decline">Decline</a>&nbsp;',
+            url=reverse('admin:account-action', args=[obj.pk]))
+
     account_actions.short_description = 'Account Actions'
     account_actions.allow_tags = True
 
@@ -85,6 +86,26 @@ class AccountAdmin(admin.ModelAdmin):
         messages.success(request,
                          mark_safe('Verification Status <b>Reset Done</b> for <{}>'.format(account.user.username)))
         return redirect('admin:api_account_changelist')
+
+    def account_action(self, request, account_id, *args, **kwargs):
+        # account.reset_verification_state()
+        # Token.objects.filter(user=account.user).delete()
+        # messages.success(request,
+        #                  mark_safe('Verification Status <b>Reset Done</b> for <{}>'.format(account.user.username)))
+        # return redirect('admin:api_account_changelist')
+        # print('NNNNNNNNNNNNNNNNNNN', self.app_l)
+        if request.method == 'POST' and request.POST.get('confirm'):
+            action = request.POST.get('action')
+            if action == 'reset':
+                return self.reset_identity_verification(request, account_id)
+            elif action == 'approve':
+                return self.approve_identity_verification(request, account_id)
+            elif action == 'decline':
+                return self.decline_identity_verification(request, account_id)
+        else:
+            account = get_object_or_404(Account, pk=account_id)
+            action = request.GET.get('action')
+            return render(request, 'account_action_confirm.html', {'action': action, 'account': account, 'opts': account._meta})
 
     def approve_identity_verification(self, request, account_id, *args, **kwargs):
         account = get_object_or_404(Account, pk=account_id)
