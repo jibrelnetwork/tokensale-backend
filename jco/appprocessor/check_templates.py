ddb13e006dd35ed19d50e589aadc745efc3d0a0f
aleksey <aleksey.selikhov@gmail.com>
2017-12-04 23:57:14 +0300

    add automated check of email templates
    (https://github.com/jibrelnetwork/jco/issues/4)

---
+++ jco/appprocessor/check_templates.py
@@ -0,0 +1,111 @@
+import os
+import random
+import string
+import requests
+import logging
+from pathlib import Path
+from typing import List, Tuple, Optional, Dict
+
+from jco.appprocessor import notify
+from jco.commonconfig import config
+from jco.appprocessor.templates.subjects import TEMPLATE__SUBJECT
+
+
+# Mail-tester.com
+MAIL_TESTER__USER = "denalex"
+MAIL_TESTER__REPORT_URL = "https://www.mail-tester.com/{}-{}&format={}"
+
+class ReportType {
+    json = "json"
+    dbug = "dbug"
+}
+
+
+def get_templates_path() -> str:
+    return Path(os.path.dirname(os.path.realpath(__file__)), "templates")
+
+
+#
+# Read a template file
+#
+
+def get_template_data(template_name: str) -> Optional[str]:
+    try:
+        with open(Path(get_templates_path(), template_name), 'r') as template_file:
+            template_data=template_file.read()
+
+        return template_data
+
+    except Exception:
+        return None
+
+
+#
+# Send an email template to mail-tester.com
+#
+
+def send_email_template(template_name: str) -> Tuple[bool, Optional[str], Optional[str]]:
+    template_id = template_name + '_' + ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
+    email = "{}-{}@mail-tester.com".format(MAIL_TESTER__USER,
+                                           template_id)
+    subject = TEMPLATE__SUBJECT[template_name]
+    body = get_template_data(template_name)
+    email_files = notify._format_email_files(
+        attachments_inline=[(
+            "jibrel_logo.png",
+            Path(get_templates_path(), "jibrel_logo.png")
+        )]
+    )
+
+    logging.getLogger(__name__).info("Sending email template {}".format(template_name))
+
+    success, mail_id = notify._send_email(
+        config.EMAIL_NOTIFICATIONS__SENDER,
+        email,
+        subject,
+        body,
+        0,
+        files=email_files
+    )
+
+    return success, mail_id, template_id
+
+
+#
+# Send to test all email templates
+#
+
+def send_email_templates() -> List[str]:
+    res = list()
+    for template_name in TEMPLATE__SUBJECT:
+        success, mail_id, template_id = send_email_template(template_name)
+        if success:
+            res.append(template_id)
+
+    return res
+
+
+#
+# Get an email template's mark using mail-tester.com
+#
+
+def get_email_template_mark(template_id: str) -> Dict:
+    _url = MAIL_TESTER__REPORT_URL \
+        .format(MAIL_TESTER__USER,
+                template_id,
+                ReportType.json)
+    r = requests.get(_url)
+    r.raise_for_status()
+
+    return r.json()["displayedMark"]
+
+
+#
+# Get an email template's report url
+#
+
+def get_email_template_report(template_id: str) -> str:
+    return MAIL_TESTER__REPORT_URL \
+            .format(MAIL_TESTER__USER,
+                    template_id,
+                    ReportType.dbug)
--- jco/appprocessor/templates/subjects.py
+++ jco/appprocessor/templates/subjects.py
@@ -1,18 +1,19 @@
-account_01_01 = "Password change request"
-account_01_02 = "Your password was updated"
-account_02_01 = "ETH Address change request"
-account_02_02 = "Your ETH Address was updated"
+TEMPLATE__SUBJECT = {
+    "account_01_01.html": "Password change request",
+    "account_01_02.html": "Your password was updated",
+    "account_02_01.html": "ETH Address change request",
+    "account_02_02.html": "Your ETH Address was updated",
+    "kyc_01.html": "Completing your JNT purchase",
+    "kyc_02.html": "Unable to verify your identity",
 
-kyc_01 = "Completing your JNT purchase"
-kyc_02 = "Unable to verify your identity"
+    "registration_01.html": "Verify your email address",
+    "registration_02.html": "Accessing your Token Sale Dashboard",
 
-registration_01 = "Verify your email address"
-registration_02 = "Accessing your Token Sale Dashboard"
+    "transactions_01.html": "Your purchase of ~{{transaction_jnt_amount}} JNT was successful!",
+    "transactions_02_01.html": "JNT withdrawal request",
+    "transactions_02_02.html": "JNT withdrawal underway",
+    "transactions_03.html": "Your JNT was transferred successfully!",
+    "transactions_04.html": "Token sale closed",
 
-transactions_01 = "Your purchase of ~{{transaction_jnt_amount}} JNT was successful!"
-transactions_02_01 = "JNT withdrawal request"
-transactions_02_02 = "JNT withdrawal underway"
-transactions_03 = "Your JNT was transferred successfully!"
-transactions_04 = "Token sale closed"
-
-presale_01 = "Your Jibrel Network Tokens have arrived!"
+    "presale_01.html": "Your Jibrel Network Tokens have arrived!"
+}
