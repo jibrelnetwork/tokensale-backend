4e68d9d8eaa1ed26e393be33292017128c20606f
aleksey <aleksey.selikhov@gmail.com>
2017-12-14 19:27:50 +0300

    fix jnt balance

--- jco/api/serializers.py
+++ jco/api/serializers.py
@@ -54,12 +54,7 @@ class AccountSerializer(serializers.ModelSerializer):
         return obj.user.username
 
     def get_jnt_balance(self, obj):
-        presale_balance = PresaleJnt.objects.filter(
-            user=obj.user).aggregate(Sum('jnt_value'))['jnt_value__sum'] or 0
-        jnt_balance = (Jnt.objects.filter(transaction__address__user=obj.user,
-                                          transaction__status=TransactionStatus.success)
-                       .aggregate(Sum('jnt_value')))['jnt_value__sum'] or 0
-        return jnt_balance + presale_balance
+        return obj.get_jnt_balance()
 
     def get_verification_form_status(self, obj):
         def personal_data_filled(obj):
