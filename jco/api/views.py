ae3b1050a829364a023883c08ea0fa43903a6411
Deployer <aleksey.selikhov@gmail.com>
2017-12-14 08:43:25 +0100

    fix TransactionsListView

--- jco/api/views.py
+++ jco/api/views.py
@@ -57,7 +57,7 @@ class TransactionsListView(APIView):
 
     def get(self, request):
         txs_qs = Transaction.objects.filter(address__user=request.user).exclude(jnt=None)
-        withdrawals_qs = Withdraw.objects.filter(address__user=request.user)
+        withdrawals_qs = Withdraw.objects.filter(user=request.user)
         presale_jnt_qs = PresaleJnt.objects.filter(user=request.user)
 
         txs = TransactionSerializer(txs_qs, many=True).data
